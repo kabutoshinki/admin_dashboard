3e6b2371bfb76da5daf4f4523d3b9b420c86a2a8
Huy <kabutoshinki@gmail.com>
2023-02-14 23:32:14 +0700

    add feature  login with gmail firebase

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { DarkModeContextProvider } from "./context/darkModeContext";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <DarkModeContextProvider>
       <App />
     </DarkModeContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,36 @@
-import "./login.scss"
-
+import { useState } from "react";
+import "./login.scss";
+import { auth } from "../../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-export default Login
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        navigate("/");
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
+  return (
+    <div className="login">
+      <form onSubmit={handleLogin}>
+        <input type="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Login</button>
+        {error && <span>Wrong email password</span>}
+      </form>
+    </div>
+  );
+};
+
+export default Login;

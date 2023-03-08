f3d3b5bf58c2293274853d28d8c3135f4c877539
Huy <kabutoshinki@gmail.com>
2023-03-08 17:06:50 +0700

    re deploy

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -18,28 +18,26 @@ const Featured = () => {
         </div>
         <p className="title">Total sales made today</p>
         <p className="amount">$420</p>
-        <p className="desc">
-          Previous transactions processing. Last payments may not be included.
-        </p>
+        <p className="desc">Previous transactions processing. Last payments may not be included.</p>
         <div className="summary">
           <div className="item">
             <div className="itemTitle">Target</div>
             <div className="itemResult negative">
-              <KeyboardArrowDownIcon fontSize="small"/>
+              <KeyboardArrowDownIcon fontSize="small" />
               <div className="resultAmount">$12.4k</div>
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Last Week</div>
             <div className="itemResult positive">
-              <KeyboardArrowUpOutlinedIcon fontSize="small"/>
-              <div className="resultAmount">$12.4k</div>
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$13.4k</div>
             </div>
           </div>
           <div className="item">
             <div className="itemTitle">Last Month</div>
             <div className="itemResult positive">
-              <KeyboardArrowUpOutlinedIcon fontSize="small"/>
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
               <div className="resultAmount">$12.4k</div>
             </div>
           </div>
